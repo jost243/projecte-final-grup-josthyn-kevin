6828485bf3e546e9f12bed207e4658ed47ce9589
jost243 <92725911+jost243@users.noreply.github.com>
2022-05-02 14:09:33 +0200

    Create MostrarPregunta.vue

---
+++ web/quiz_project/src/components/MostrarPregunta.vue
@@ -0,0 +1,27 @@
+
+<script>
+  export default {
+    data() {
+      return {
+        myjson: [],      
+      };
+    },
+    mounted () {
+       console.log("patata")
+      fetch('./quiz.json')
+      .then(res => res.json())
+      .then((data) => {
+        this.myjson = data;
+        console.log(this.myjson)
+       
+      });
+    },
+  }
+
+</script>
+
+<template>
+
+
+
+</template>
